ffa5562b271edc2f1e9cc41aba7a96743a7fe9ca
rauliesan <raullsdev@gmail.com>
2025-11-27 18:16:53 +0100

    Changing the style

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
+// Archivo: pages/_app.js
+
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Comparador de DMs con IA</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        {/* Usamos las fuentes MedievalSharp y Lora para el estilo de fantasía */}
+        <link href="https://fonts.googleapis.com/css2?family=MedievalSharp&family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet" />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
 
